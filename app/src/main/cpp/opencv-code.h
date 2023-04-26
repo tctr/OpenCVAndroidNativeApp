9a1e16eace11905fde01c63bb3b8f829afd06af7
tctr <thierry.coutier@gmail.com>
2023-04-26 10:03:58 +0200

    add the cpp code
    corrections

--- app/src/main/cpp/opencv-code.h
+++ app/src/main/cpp/opencv-code.h
@@ -1,4 +1,14 @@
 #ifndef OPENCVNATIVEAPP_OPENCV_CODE_H
 #define OPENCVNATIVEAPP_OPENCV_CODE_H
 
+#pragma once
+
+#include <opencv2/core.hpp>
+
+using namespace cv;
+
+void myFlip(Mat src);
+void myBlur(Mat src, float sigma);
+
+
 #endif //OPENCVNATIVEAPP_OPENCV_CODE_H
